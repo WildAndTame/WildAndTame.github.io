c1d9a09dffb0e5a776f73b40125c5ae9c7e3dfe1
jinalex <jin.alex123@gmail.com>
2016-03-14 17:04:25 -0400

    email test

--- app.js
+++ app.js
@@ -6,4 +6,12 @@ var email = "email@example.com";
 // URL of the web app to POST
 var url = "https://script.google.com/macros/s/AKfycbzkTSD_-TPK6gWSL4MD9AQuMBwR7T-R2mb6lXz56SV5D3mvHFs/exec";
 
-$.post( url, $( "#email" ).serialize());
+$.post( url, email);
+
+var userEmail = $( "#email" ).serialize();
+
+
+function function_name(userEmail) {
+    alert(userEmail);
+    // $.post( url, userEmail);
+}
